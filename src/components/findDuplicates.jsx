ff8f47a465b384e9a4d768129434dda4f7133be9
Ricardo van der Heijden <ricardovdheijden@hotmail.com>
2019-04-09 21:26:26 +0200

    PM-001 Stores trackIds from Spotify Urls in state-object

--- src/components/findDuplicates.jsx
+++ src/components/findDuplicates.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const FindDuplicates = () => {
-    return(
-        <h2>Find Duplicates</h2>
-    );
-};
+class FindDuplicates extends Component {
+    state = {
+        trackIds: []
+    };
+
+    handleChange = event => {
+        let trackIds = event.target.value.split('\n')
+            .filter(c => {
+                return c.length > 31 && c.startsWith('https://open.spotify.com/track/')
+            })
+            .map(c => {
+                return c.substring(c.lastIndexOf('/') + 1)
+            });
+
+        this.setState({trackIds: trackIds});
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <h2>Find Duplicates</h2>
+                <textarea className="span6"
+                          rows="10"
+                          cols="70"
+                          placeholder={'https://open.spotify.com/track/3hMHG6lx9QHVcfYSUr5PoM\nhttps://open.spotify.com/track/3O9zeBmAi5JRBMSpIQGx2v'}
+                          required
+                          onChange={this.handleChange}/>
+            </React.Fragment>
+        );
+    }
+
+}
 
 export default FindDuplicates;
