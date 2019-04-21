e569b929869ebb79d064cc53bedd59b45a48c4dc
Ricardo van der Heijden <ricardovdheijden@hotmail.com>
2019-04-21 17:40:37 +0200

    PM-001 Cleans up React starter files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import FindDuplicates from './components/findDuplicates';
 import NavBar from './components/navbar';
-import './App.css';
 
 class App extends Component {
   render() {
