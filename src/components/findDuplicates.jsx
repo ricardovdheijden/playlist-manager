8d1edc0076fa335376080fc13fd2c7fad27cde28
Ricardo van der Heijden <ricardovdheijden@hotmail.com>
2019-04-21 14:18:30 +0200

    PM-001 Adds condition to only show the table when there is input given

--- src/components/findDuplicates.jsx
+++ src/components/findDuplicates.jsx
@@ -15,27 +15,31 @@ class FindDuplicates extends Component {
                           placeholder={'https://open.spotify.com/track/3hMHG6lx9QHVcfYSUr5PoM\nhttps://open.spotify.com/track/3O9zeBmAi5JRBMSpIQGx2v'}
                           required
                           onChange={this.handleChange}/>
-                <h5>Duplicates</h5>
-                <table className="table table-sm">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Track ID</th>
-                            <th scope="col">Duplicates</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.playlist.map((track, pos) =>
-                            <tr key={pos + 1}>
-                                <th scope="row">{pos + 1}</th>
-                                <td>
-                                    <a href={'https://open.spotify.com/track/' + track.trackId} target="_blank">{track.trackId}</a>
-                                </td>
-                                <td>{track.duplicates.map(tracknumber => tracknumber + 1).join(', ')}</td>
+                { this.state.playlist.length ?
+                    <React.Fragment>
+                        <h5>Duplicates</h5>
+                        <table className="table table-sm">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Track ID</th>
+                                <th scope="col">Duplicates</th>
                             </tr>
-                        )}
-                    </tbody>
-                </table>
+                            </thead>
+                            <tbody>
+                            { this.state.playlist.map((track, pos) =>
+                                <tr className={track.duplicates.length ? 'table-secondary':null} key={pos + 1}>
+                                    <th scope="row">{pos + 1}</th>
+                                    <td>
+                                        <a href={'https://open.spotify.com/track/' + track.trackId} target="_blank">{track.trackId}</a>
+                                    </td>
+                                    <td>{track.duplicates.map(tracknumber => tracknumber + 1).join(', ')}</td>
+                                </tr>
+                            )}
+                            </tbody>
+                        </table>
+                    </React.Fragment> : null
+                }
             </React.Fragment>
         );
     }
